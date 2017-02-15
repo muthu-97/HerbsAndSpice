45d911ceddfe9bccfdefc95bb0e25ffd61ef508d
Muthuraman Lakshmanan <Muthuraman Lakshmanan>
2017-02-15 15:02:43 +0530

    error correction

--- JS/bill.js
+++ JS/bill.js
@@ -13,7 +13,7 @@ function populate(data)
     var tot=0;var str="";
 for (var i = 0; i < data.order.length; i=i+1) {
   tot+=parseInt(data.order[i].total);
-  str+='<tr><td class="col-md-9"><em>'+data.order[i].name+'</em></h4></td><td class="col-md-1" style="text-align: center">'+data.array[0].order[i].nos+'</td>'+
+  str+='<tr><td class="col-md-9"><em>'+data.order[i].name+'</em></h4></td><td class="col-md-1" style="text-align: center">'+data.order[i].nos+'</td>'+
   '<td class="col-md-1 text-center">'+data.order[i].price+'</td>'+
 '<td class="col-md-1 text-center">'+data.order[i].total+'</td>      </tr>'
 }
